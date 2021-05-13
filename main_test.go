3d06c5daf3faa5272bbed5c0873fa0bf5de1c02b
Santhosh M <72694672+ssanmaru@users.noreply.github.com>
2021-05-13 13:16:07 +0530

    Updated main test go to print date time to console

--- main_test.go
+++ main_test.go
@@ -1,6 +1,20 @@
 package main
 
-import "testing"
+import (
+    "fmt"
+    "time"
+  "testing"
+)
 
 func TestThis(t *testing.T) {
+  fmt.Println("hello there")
+  t := time.Now()
+    y, mon, d := t.Date()
+    h, m, s := t.Clock()
+    fmt.Println("Year: ", y)
+    fmt.Println("Month: ", mon)
+    fmt.Println("Day: ", d)
+    fmt.Println("Hour: ", h)
+    fmt.Println("Minute: ", m)
+    fmt.Println("Second: ", s)
 }
